1bcf8b16ffe478062a40b25cf095f163a8dffdd8
Selina Erhabor <selinaerhabor@gmail.com>
2019-02-05 15:04:42 +0000

    First recipe document has been added into the database via mLab, which is now successfully connected to the online-cooking app. Each property has been called in the recipes.html template and is appearing when the app is run

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"]  = 'online-cookbook'
-app.config["MONGO_URI"] = 'mongodb://admin:u537@6m1n@ds213665.mlab.com:13665/online-cookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:u537a6m1n@ds213665.mlab.com:13665/online-cookbook'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def Hello():
-    return 'This application is up and running'
-    
+@app.route('/load_recipes')
+def load_recipes():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find())
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
