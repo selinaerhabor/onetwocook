d8f51b511b572b83dba98fc074feab600e4722c3
Selina Erhabor <selinaerhabor@gmail.com>
2019-03-19 17:51:34 +0000

    User is being successfully redirected to correct pages on the website. Data from the database is also correctly displaying in their respective pages making the title of page match the content displayed from the database.

--- app.py
+++ app.py
@@ -12,31 +12,39 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("cuisines.html",
+    return render_template('cuisines.html',
     cuisines=mongo.db.cuisines.find())
 
-@app.route('/addrecipe')
+@app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html")
+    return render_template('addrecipe.html')
 
-@app.route('/managerecipes')
+@app.route('/manage_recipes')
 def manage_recipes():
-    return render_template("managerecipes.html")
+    return render_template('managerecipes.html')
     
-@app.route('/editrecipe')
+@app.route('/edit_recipe')
 def edit_recipe():
-    return render_template("editrecipe.html")
+    return render_template('editrecipe.html')
     
-@app.route('/listofrecipes')
-def list_of_recipes():
-    return render_template("listofrecipes.html",
-    recipes=mongo.db.recipes.find())
+@app.route('/list_of_recipes/<cuisine_type>')
+def list_of_recipes(cuisine_type):
+    return render_template('listofrecipes.html',
+    recipes=mongo.db.recipes.find({'cuisine_type': cuisine_type}))
     
     
-@app.route('/load_recipe')
-def load_recipe():
-    return render_template("recipe.html",
-    recipes=mongo.db.recipes.find({}))
+@app.route('/load_recipe/<recipe_name>')
+def load_recipe(recipe_name):
+    return render_template('recipe.html',
+    recipes=mongo.db.recipes.find({'recipe_name': recipe_name}))
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
